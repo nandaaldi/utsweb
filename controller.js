971d44d55d230185584c3aec0d5d67a5af384991
Nanda Aldi <nandhambalelo@gmail.com>
2020-05-20 15:09:14 +0700

    tampil data berdasarkan id

--- controller.js
+++ controller.js
@@ -26,4 +26,52 @@ exports.tampildatamontir = function(req,res){
         }
     });
 };
+//menampilkan data sparepart dan montir berdasarkan id
+exports.tampildatasparepartid = function (req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_sparepart WHERE id_sparepart = ?', [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res);
+        }
+    });
+};
+
+exports.tampildatamontirid = function (req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_montir WHERE id_montir = ?', [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res);
+        }
+    });
+};
+
+//Menampilkan Data Service 
+exports.tampilservice = function(req,res){
+    
+    connection.query('SELECT t_user.username, t_service.tgl_service, t_montir.Nama_montir, t_sparepart.nama_sparepart,t_sparepart.harga_sparepart, t_service.jumlah_sparepart, t_service.jam_service, t_montir.harga_perjam, t_service.total_service FROM t_service JOIN t_user JOIN t_sparepart JOIN t_montir WHERE t_service.id_user = t_user.id_user AND t_service.id_sparepart = t_sparepart.id_sparepart AND t_service.id_montir = t_montir.id_montir ORDER BY t_user.id_user ',
+     function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan data service total
+exports.tampildatamontir = function(req,res){
+    connection.query('SELECT * FROM t_montir', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
 
